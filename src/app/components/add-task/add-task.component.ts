2ba9f984b994298d2570fed3015695876d98363d
muhdamean <muhdamean@gmail.com>
2021-12-01 16:29:52 +0100

    initial commit

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css']
+})
+export class AddTaskComponent implements OnInit {
+  @Output() onAddTask:EventEmitter<Task>=new EventEmitter()
+text:string="";
+day:string="";
+showAddTask!:boolean;
+reminder:boolean=false;
+subscription: Subscription;
+
+  constructor(private uiService:UiService) { 
+    this.subscription=this.uiService
+    .onToggle()
+    .subscribe((value)=>(this.showAddTask=value));
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(){
+    if(!this.text){
+      alert('please add a task');
+      return;
+    }
+
+    const newTask={
+      text:this.text,
+      day:this.day,
+      reminder:this.reminder
+    }
+
+    this.onAddTask.emit(newTask);
+    //@todo - emit event
+    this.text='';
+    this.day='';
+    this.reminder=false;
+  }
+  
+}
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+  currentYear:number=new Date().getFullYear();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getYear() {
+    return this.currentYear;
+  }
+}
